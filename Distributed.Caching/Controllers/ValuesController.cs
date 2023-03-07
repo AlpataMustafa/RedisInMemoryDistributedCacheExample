70b984743e160c843759e481c2d7b429ce83e199
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-03-08 00:18:44 +0300

    IDistributedCache reference added and records added to redis cache

---
+++ Distributed.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+
+namespace Distributed.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly IDistributedCache distributedCache;
+
+        public ValuesController(IDistributedCache distributedCache)
+        {
+            this.distributedCache = distributedCache;
+        }
+
+        [HttpGet("set")]
+        public async Task<IActionResult> Set(string name, string surname)
+        {
+            await distributedCache.SetStringAsync("name", name, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30), 
+                SlidingExpiration = TimeSpan.FromSeconds(10)
+            });
+            await distributedCache.SetAsync("surname", Encoding.UTF8.GetBytes(surname));
+            return Ok();
+        }
+
+        [HttpGet("get")]
+        public async Task<IActionResult> Get()
+        {
+            var name = await distributedCache.GetStringAsync("name");
+            var nameBinary = await distributedCache.GetAsync("surname");
+            var surname = Encoding.UTF8.GetString(nameBinary);
+            return Ok(new
+            {
+                name,
+                nameBinary,
+                surname
+            });
+        }
+    }
+}
