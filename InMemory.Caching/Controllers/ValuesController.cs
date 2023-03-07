d30d7912f7e3500c6a53be9c19ff9e468504824c
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-03-07 23:58:25 +0300

    Absolute and sliding time options added

--- InMemory.Caching/Controllers/ValuesController.cs
+++ InMemory.Caching/Controllers/ValuesController.cs
@@ -17,15 +17,31 @@ namespace InMemory.Caching.Controllers
         [HttpGet]
         public void Set(string name)
         {
-            memoryCache.Set("name",name);
+            memoryCache.Set("name", name);
         }
         [HttpGet]
         public string Get()
         {
-            if (memoryCache.TryGetValue<string>("name",out string name)==true)
+            if (memoryCache.TryGetValue<string>("name", out string name) == true)
             {
                 return name.ToLower();
             }
+            return "";
+        }
+
+        [HttpGet]
+        public void SetDate()
+        {
+            memoryCache.Set<DateTime>("date", DateTime.Now, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(10)
+            });
+        }
+        [HttpGet]
+        public DateTime GetDate()
+        {
+            return memoryCache.Get<DateTime>("date");
         }
     }
 }
