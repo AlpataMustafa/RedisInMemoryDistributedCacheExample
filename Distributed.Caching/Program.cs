da7cabec71b4b483dfb0010f5abf339bbce97102
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-03-08 00:18:36 +0300

    AddStackExchangeRedisCache service added to program.cs

--- Distributed.Caching/Program.cs
+++ Distributed.Caching/Program.cs
@@ -1,15 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddStackExchangeRedisCache(x => x.Configuration = "localhost:6379");
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
