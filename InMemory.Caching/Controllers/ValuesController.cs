80575c1f25790f99c87c1b439c0c2ca02cf6dea8
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-03-07 23:44:54 +0300

    Set and get endpoints added to the controller

--- InMemory.Caching/Controllers/ValuesController.cs
+++ InMemory.Caching/Controllers/ValuesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace InMemory.Caching.Controllers
 {
@@ -7,5 +8,21 @@ namespace InMemory.Caching.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        readonly IMemoryCache memoryCache;
+
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            this.memoryCache = memoryCache;
+        }
+        [HttpGet]
+        public void Set()
+        {
+            memoryCache.Set("name", "emre");
+        }
+        [HttpGet]
+        public string Get()
+        {
+            return memoryCache.Get<string>("name");
+        }
     }
 }
