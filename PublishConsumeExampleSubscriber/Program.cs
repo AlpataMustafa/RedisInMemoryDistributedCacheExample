b473d7b872eafceeffd453015f8c98949bf5498b
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-03-09 23:39:44 +0300

    Subscription operations added

--- PublishConsumeExampleSubscriber/Program.cs
+++ PublishConsumeExampleSubscriber/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:6379");
+ISubscriber subscriber = connection.GetSubscriber();
+
+await subscriber.SubscribeAsync("mychannel",(channel, message)=>{
+    Console.WriteLine(message);
+});
+Console.Read();
