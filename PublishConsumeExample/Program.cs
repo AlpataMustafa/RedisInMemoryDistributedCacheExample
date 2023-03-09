45a651d97d9193039358c1e7b33953bb9b4197bb
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-03-09 22:14:01 +0300

    subscriber operations done

--- PublishConsumeExample/Program.cs
+++ PublishConsumeExample/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:6379");
+ISubscriber subscriber = connection.GetSubscriber();
+
+while (true)
+{
+    Console.WriteLine("Mesaj: ");
+    string message = Console.ReadLine();
+    await subscriber.PublishAsync("mychannel", message);
+}
